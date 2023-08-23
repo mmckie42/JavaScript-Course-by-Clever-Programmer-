378c465f0cdc14450227425f9713db7c31fd1c31
Michael <michael.mckie@gpsupport.com.au>
2023-08-23 16:12:37 +1000

    1st vid playground

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,1 +1,15 @@
 console.log('hello world');
+
+
+//operators
+// fruit = prompt('what is your favorite fruit')
+// console.log(fruit)
+food = Number(prompt('How much does your food cost'))
+tipPercentage = Number(prompt('tip percentage?') / 100)
+tipAmount = food * tipPercentage
+totalAmount = tipAmount + food
+console.log(tipPercentage)
+alert(`total amount will be ${totalAmount}`)
+
+//user input
+
