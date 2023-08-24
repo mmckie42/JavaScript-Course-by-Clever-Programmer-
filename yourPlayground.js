df5383d23df576002b7260c0c5a054edf0123313
Michael <michael.mckie@gpsupport.com.au>
2023-08-24 22:50:37 +1000

    baby weather app section completed

--- yourPlayground.js
+++ yourPlayground.js
@@ -4,12 +4,27 @@ console.log('hello world');
 //operators
 // fruit = prompt('what is your favorite fruit')
 // console.log(fruit)
-food = Number(prompt('How much does your food cost'))
+
+/*
+
 tipPercentage = Number(prompt('tip percentage?') / 100)
 tipAmount = food * tipPercentage
 totalAmount = tipAmount + food
 console.log(tipPercentage)
 alert(`total amount will be ${totalAmount}`)
+*/
 
 //user input
+// food = Number(prompt('How much does your food cost'))
+
+// Baby weather app
+// if rain then grab umbrella
+// else where your sunglasses
+// let weather
+// if (weather == 'rain') {
+//     console.log('Grab your umbrella')       
+// } else {
+//     console.log('Wear your sunglasses')
+// }
+
 
